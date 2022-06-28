63d1e2e23a106ca10f5f826d8833aac376e0edc1
unknown <hassankhan5805@gmail.com>
2022-06-28 16:16:45 -0700

    UI updated

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
+import 'dart:ui';
 
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/services.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -22,6 +24,7 @@ class _HomeState extends State<Home> {
 
   @override
   void initState() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
     updateFcm();
     super.initState();
   }
@@ -33,12 +36,39 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: Text("Electric Bike Status"),
+          centerTitle: true,
+          backgroundColor: Colors.black,
+        ),
+        backgroundColor: Colors.black,
         body: StreamBuilder(
             stream: databaseReference.child('status').onValue,
             builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
               if (snapshot.hasData) {
                 final map = snapshot.data!.snapshot.value;
-                return Center(child: Text('${map}'));
+                return Center(
+                    child: Container(
+                        width: 200,
+                        height: 80,
+                        decoration: BoxDecoration(
+                          color: map == "true" ? Colors.green : Colors.red,
+                          boxShadow: [
+                            BoxShadow(
+                              color: map == "true" ? Colors.green : Colors.red,
+                              offset: Offset(0, 0),
+                              blurRadius: 36,
+                              spreadRadius: 16,
+                            )
+                          ],
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Center(
+                          child: Text(
+                            '${map == "true" ? "Stand Up" : "Stand Down"}',
+                            style: TextStyle(color: Colors.white, fontSize: 22),
+                          ),
+                        )));
               } else if (snapshot.hasError) {
                 return Center(child: Text(snapshot.error.toString()));
               } else {
